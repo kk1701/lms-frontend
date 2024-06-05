6544cdc2e34f6e4b60d07daa5b1e218142951c59
kk1701 <karankute05@gmail.com>
2024-06-05 10:32:03 +0530

    added: redux and axios setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import './index.css'
 
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
+import store from './redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
